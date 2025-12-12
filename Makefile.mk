9ab0851b21454f70919e08ce31f02036b4fcca26
wh0smarg0 <wh0smarg0.s@gmail.com>
2025-12-12 10:43:40 +0000

    up to date

--- Makefile.mk
+++
@@ -1,9 +0,0 @@
-obj-m += hello1.o hello2.o
-
-ccflags-y := -I$(src)/inc
-
-all:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) modules
-
-clean:
-	make -C /lib/modules/$(shell uname -r)/build M=$(PWD) clean
